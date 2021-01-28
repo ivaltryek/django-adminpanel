a32d9701f0b8bc08f282297c113e4666ee5f5953
Meet Vasani <meet.vasani86@gmail.com>
2021-01-28 13:12:40 +0530

    Added local static server configuration.

--- adminpanel/adminpanel/settings.py
+++ adminpanel/adminpanel/settings.py
@@ -118,3 +118,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- adminpanel/adminpanel/urls.py
+++ adminpanel/adminpanel/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
+
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) --> To host static server in local development.
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('admin/', admin.site.urls),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
